34fcd4fb2dfc87a0ea1d21c2dab9124f38c8f332
Abdo Rakhma <abdelghafour.rakhma@info.nl>
2019-04-12 16:34:48 +0200

    tweaking heroku deploy

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Doing your staging deployment to heroku...."
 docker push registry.heroku.com/node-docker-travis/web
+heroku container:release web --app node-docker-travis
 echo "Done! deployed to Heroku SUCCESSFULLY"
