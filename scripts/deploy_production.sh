27f666e28349f48cefc7bb3170d648a1bf0e8048
Abdo Rakhma <abdelghafour.rakhma@info.nl>
2019-04-12 17:10:17 +0200

    Refactoring deploy scripts

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Doing your production deployment to DockerHub...."
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push rakhma/backend_nodejs
 echo "Done! deployed to DockerHub SUCCESSFULLY"
--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Doing your staging deployment to heroku...."
+docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
 docker push registry.heroku.com/node-docker-travis/web
 heroku container:release web --app node-docker-travis
 echo "Done! deployed to Heroku SUCCESSFULLY"
