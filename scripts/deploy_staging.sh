11bbeba6efee9fa30b9002cf14295d0ccb103500
Abdo Rakhma <abdelghafour.rakhma@info.nl>
2019-04-13 11:59:24 +0200

    update staging script

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Doing your staging deployment to heroku...."
-docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
+#docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
+echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
 docker push registry.heroku.com/node-docker-travis/web
 heroku container:release web --app node-docker-travis
 echo "Done! deployed to Heroku SUCCESSFULLY"
