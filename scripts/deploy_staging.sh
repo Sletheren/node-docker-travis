f923535b5f49b11b8919197c91d2d628bb8f5012
Abdo Rakhma <abdelghafour.rakhma@info.nl>
2019-04-13 20:51:18 +0200

    Add AWS deployment

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Doing your staging deployment to heroku...."
-#docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
 echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
 docker push registry.heroku.com/node-docker-travis/web
 heroku container:release web --app node-docker-travis
